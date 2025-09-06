23f6d591ec23b7eea642b07eabda2081d7e42d53
Ariq Farhan <ariqalthaf445@gmail.com>
2025-09-06 11:19:57 +0700

    add .gitignore and update main.go to load environment variables and initialize database connection

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"coding/backend-api/config"
+	"coding/backend-api/database"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Memuat variabel lingkungan dari file .env
+	config.LoadEnv()
+
+	// Inisialisasi koneksi database
+	database.InitDB()
+
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+
+		c.JSON(200, gin.H{
+			"message": "halo Ini Dari Golang!",
+		})
+	})
+
+	router.Run(": " + config.GetEnv("APP_PORT", "3000"))
 }
